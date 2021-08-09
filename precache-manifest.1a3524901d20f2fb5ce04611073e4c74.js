a6cffd717e63f488fe64c2b1d200ea74039aa526
Sukanta Dinda <dindasukanta19@gmail.com>
2021-08-09 22:31:30 +0530

    Updates

---
+++ precache-manifest.1a3524901d20f2fb5ce04611073e4c74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64ca1a496a772dc5cdeda750e8770dc8",
+    "url": "/meme-creator-app/index.html"
+  },
+  {
+    "revision": "c01f3ab158198d95bdb6",
+    "url": "/meme-creator-app/static/css/main.f9684c15.chunk.css"
+  },
+  {
+    "revision": "8fb7ee323cc9a4512a5f",
+    "url": "/meme-creator-app/static/js/2.c5bf39bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/meme-creator-app/static/js/2.c5bf39bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c01f3ab158198d95bdb6",
+    "url": "/meme-creator-app/static/js/main.9fe3535d.chunk.js"
+  },
+  {
+    "revision": "8bb1286797332e0d5e1c",
+    "url": "/meme-creator-app/static/js/runtime-main.3a0cbdf0.js"
+  }
+]);
